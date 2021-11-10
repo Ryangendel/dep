dc3c988e5581122c99f0dacb8875cead4494ed3d
ryan gendel <ryangendel@ryans-MacBook-Pro.local>
2021-11-10 11:27:12 -0700

    added server

---
+++ server.js
@@ -0,0 +1,19 @@
+const { strictEqual } = require("assert")
+const express = require("express")
+
+const app = express()
+
+const path = require("path")
+
+const PORT = process.env.PORT || 3001
+
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("build"))
+    app.get("*", (req,res)=>{
+        req.sendFile(path.resolve(__dirname, "build","index.html"))
+    })
+}
+
+app.listen(PORT,()=>{
+    console.log("listening on port 3001")
+})
