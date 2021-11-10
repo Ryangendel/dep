664e22518d9ea45676ae11b1e32ae38fa4fa07bd
ryan gendel <ryangendel@ryans-MacBook-Pro.local>
2021-11-10 13:16:05 -0700

    message

--- server.js
+++ server.js
@@ -14,10 +14,34 @@ if (process.env.NODE_ENV === "production"){
     })
 }
 
-app.get("/bulldog",(req, res)=>{
+app.use("/bulldog",(req, res)=>{
     res.send({hi:"FROM SERVER"})
 })
 
 app.listen(PORT,()=>{
     console.log("listening on port 3001")
 })
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const path = require('path')
+// const app = express();
+
+
+// app.use(bodyParser.json());
+// app.use(cors());
+
+// // API
+// // const users = require('/api/users');
+// app.use('/api/users', users);
+
+// app.use(express.static(path.join(__dirname, '../build')))
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../build'))
+// })
+// -----
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//     console.log(`Server started on port ${port}`);
+// });
